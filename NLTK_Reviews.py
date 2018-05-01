e6f971e019d788f088a9bddb5b12774043e3274c
Umesh <37169232+dinesh92x@users.noreply.github.com>
2018-05-01 19:14:00 +0530

    Update NLTK_Reviews.py

--- NLTK_Reviews.py
+++ NLTK_Reviews.py
@@ -12,7 +12,8 @@ from nltk.stem.porter import PorterStemmer
 corpus=[]
 
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
-    
+
+# cleaning the text
 for i in range(0,1000):
     # Importing the dataset
     review=re.sub(r'[^a-zA-z]',' ',dataset['Review'][i])
@@ -22,47 +23,42 @@ for i in range(0,1000):
     review=[ps.stem(word) for word in review if word not in set(stopwords.words('english'))]
     review=' '.join(review)
     corpus.append(review)
-    
+
+# converting text into int and making sparce matrix
 from sklearn.feature_extraction.text import CountVectorizer
 cv=CountVectorizer(max_features=1500)
 x=cv.fit_transform(corpus).toarray()
 y=dataset.iloc[:,1].values
 
+# spliting into train and test
 from sklearn.cross_validation import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=0)
 
+# applying classifier Naive Bayes
 from sklearn.naive_bayes import GaussianNB
 classifier=GaussianNB()
 classifier.fit(x_train,y_train)
 y_pred=classifier.predict(x_test)
 
+# for fiding accuracy
 from sklearn.metrics import confusion_matrix
-
 cm=confusion_matrix(y_test,y_pred)
-from sklearn.metrics import accuracy
 
-# apply Rednom Forest
-from sklearn.ensemble import RandomForestClassifier
-
+# apply Rednom Forest an another classifier
+from sklearn.ensemble import RandomForestClassifier # we are going to apply three classifier
+                                                    # for finding best accuracy
 classifier1=RandomForestClassifier()
 classifier1.fit(x_train,y_train)
 y_pred=classifier1.predict(x_test)
 cm_forest=confusion_matrix(y_test,y_pred)
 
-# Apply KNN
+# Apply KNN an another classifier 
 from sklearn.neighbors import KNeighborsClassifier
 classifier2=KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
-
 classifier2.fit(x_train,y_train)
 y_pred_KNN=classifier.predict(x_test)
-
 cm_KNN=confusion_matrix(y_test,y_pred_KNN)
 
-
-
-
-
-
-
-
-
+# WE come to this point that ,,,
+#Random Forest gave us 85.3 % accuracy
+#knn gave 73% accuracy
